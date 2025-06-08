ae2398f3463bab9ab58eaf9b89118b89caf4708b
yjhwang <hghyj719@gmail.com>
2025-06-08 23:28:23 +0900

    feat: v2L configs

--- efficientnet_v2/train.sh
+++ efficientnet_v2/train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=infer_test
-#SBATCH --nodelist=hpe162
+#SBATCH --nodelist=hpe159
 #SBATCH --gpus=8
 #SBATCH --cpus-per-task=20
 #SBATCH --mem-per-cpu=8G
@@ -10,6 +10,11 @@
 echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX "
 echo "Job name:= " "$SLURM_JOB_NAME"
 echo "Nodelist:= " "$SLURM_JOB_NODELIST"
+echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX "
+
+echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX "
+echo "실험 내용"
+echo "v2l 모델 실험, phase2 - lr 0.008, batch 32, phase1 epoch 50" 
 echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX "
 
 echo "Run started at:- "
